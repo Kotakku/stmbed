8ba641802118e5ace6a8ca3248befd8972850f5c
Kotakku <kotakkucu@gmail.com>
2024-05-17 23:09:51 +0900

    Feat FDCAN Classic mode

--- callbacks/can_callback.cpp
+++ callbacks/can_callback.cpp
@@ -2,7 +2,6 @@
 
 #ifdef HAL_CAN_MODULE_ENABLED
 #ifndef STMBED_MANUAL_CAN_CB
-
 #ifdef CAN_RX_FIFO0
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
     using namespace stmbed;
@@ -17,6 +16,26 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
     }
 }
 #endif // CAN_RX_FIFO0
-
 #endif // STMBED_MANUAL_CAN_CB
 #endif // HAL_CAN_MODULE_ENABLED
+
+#include <stdio.h>
+
+#ifdef HAL_FDCAN_MODULE_ENABLED
+#ifndef STMBED_MANUAL_CAN_CB
+#ifdef FDCAN_RX_FIFO0
+// コールバックの実装
+void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs) {
+    using namespace stmbed;
+    FDCAN_RxHeaderTypeDef rx_header;
+    CANMessage msg;
+
+    if (HAL_FDCAN_GetRxMessage(hfdcan, FDCAN_RX_FIFO0, &rx_header, msg.data.data()) == HAL_OK) {
+        msg.id = rx_header.Identifier;
+        msg.size = rx_header.DataLength;
+        callback::callback<CAN::CallbackFnType>(reinterpret_cast<intptr_t>(hfdcan), msg);
+    }
+}
+#endif // FDCAN_RX_FIFO0
+#endif // STMBED_MANUAL_CAN_CB
+#endif // HAL_FDCAN_MODULE_ENABLED
--- can.hpp
+++ can.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include "main.h"
+
 #include "callbacks/callback.hpp"
 #include <array>
 #include <cstring>
@@ -30,13 +32,13 @@ struct CANMessage {
     uint32_t id;                 // 11bit or 29bit identifier
     std::array<uint8_t, 8> data; // Data field
     uint8_t size;                // Length of data field in bytes
-    CANFormat format;
+    CANFormat format = CANStandard;
     // CANType        type;
 };
 
 } // namespace stmbed
 
-#ifdef HAL_CAN_MODULE_ENABLED
+#if defined(HAL_CAN_MODULE_ENABLED) || defined(HAL_FDCAN_MODULE_ENABLED)
 
 #include "main.h"
 #include <functional>
@@ -47,15 +49,33 @@ namespace stmbed {
 
 class CAN {
 public:
+#if defined(HAL_CAN_MODULE_ENABLED)
+    using CanHandleType = CAN_HandleTypeDef;
+#elif defined(HAL_FDCAN_MODULE_ENABLED)
+    using CanHandleType = FDCAN_HandleTypeDef;
+#endif
+
     using CallbackFnType = void(const CANMessage &msg);
 
-    CAN(CAN_HandleTypeDef *handle) : handle_(handle) {
+    CAN(CanHandleType *handle) : CAN(handle, 0, 0) {}
+    HAL_StatusTypeDef write(CANMessage &msg, bool wait_mailbox_free = true) {
+        return write(msg.id, msg.data.data(), msg.size, msg.format == CANFormat::CANExtended, wait_mailbox_free);
+    }
+
+    // int read(CANMessage &msg);
+
+    // 受信割り込み
+    void attach(std::function<CallbackFnType> &&fn, uint8_t priority = 100) {
+        callback::attach(reinterpret_cast<intptr_t>(handle_), std::move(fn), priority);
+    }
+
+#if defined(HAL_CAN_MODULE_ENABLED)
+    CAN(CAN_HandleTypeDef *handle, uint32_t filter_id, uint32_t filter_mask) : handle_(handle) {
         CAN_FilterTypeDef filter;
-        filter.FilterIdHigh = 0;
-        filter.FilterIdLow = 0;
-        filter.FilterMaskIdHigh = 0;
-        filter.FilterMaskIdLow = 0;
-        filter.FilterScale = CAN_FILTERSCALE_32BIT;
+        filter.FilterIdHigh = filter_id << 5;
+        filter.FilterIdLow = filter_id << 21;
+        filter.FilterMaskIdHigh = filter_mask << 5;
+        filter.FilterMaskIdLow = filter_mask << 21;
         filter.FilterFIFOAssignment = CAN_FILTER_FIFO0;
         filter.FilterBank = 0;
 
@@ -64,6 +84,7 @@ public:
             filter.FilterBank = 14;
 #endif
 
+        filter.FilterScale = CAN_FILTERSCALE_32BIT;
         filter.FilterMode = CAN_FILTERMODE_IDMASK;
         filter.SlaveStartFilterBank = 14;
         filter.FilterActivation = ENABLE;
@@ -80,35 +101,78 @@ public:
         }
     }
 
-    HAL_StatusTypeDef write(const CANMessage &msg, bool wait_mailbox_free = true) {
+    HAL_StatusTypeDef write(uint32_t id, uint8_t *data, uint32_t size, bool is_extended_frame, bool wait_mailbox_free) {
         CAN_TxHeaderTypeDef TxHeader;
-        if (msg.format == CANFormat::CANExtended) {
-            TxHeader.ExtId = msg.id;
+        if (is_extended_frame) {
+            TxHeader.ExtId = id;
         } else {
-            TxHeader.StdId = msg.id;
+            TxHeader.StdId = id;
         }
         TxHeader.RTR = CAN_RTR_DATA;
-        TxHeader.IDE = (msg.format == CANFormat::CANStandard) ? CAN_ID_STD : CAN_ID_EXT;
-        TxHeader.DLC = msg.size;
+        TxHeader.IDE = is_extended_frame ? CAN_ID_EXT : CAN_ID_STD;
+        TxHeader.DLC = size;
         TxHeader.TransmitGlobalTime = DISABLE;
         uint32_t TxMailbox;
 
         while (wait_mailbox_free && HAL_CAN_GetTxMailboxesFreeLevel(handle_) == 0)
             ;
-        return HAL_CAN_AddTxMessage(handle_, &TxHeader, (uint8_t *)msg.data.data(), &TxMailbox);
+        return HAL_CAN_AddTxMessage(handle_, &TxHeader, data, &TxMailbox);
+    }
+#endif
+
+#if defined(HAL_FDCAN_MODULE_ENABLED)
+    CAN(FDCAN_HandleTypeDef *handle, uint32_t filter_id, uint32_t filter_mask) : handle_(handle) {
+        if (handle_->State == HAL_FDCAN_STATE_READY) {
+            FDCAN_FilterTypeDef filter;
+            filter.IdType = FDCAN_STANDARD_ID;
+            filter.FilterIndex = 0;
+            filter.FilterType = FDCAN_FILTER_MASK;
+            filter.FilterConfig = FDCAN_FILTER_TO_RXFIFO0;
+            filter.FilterID1 = filter_id;
+            filter.FilterID2 = filter_mask;
+
+            if (HAL_FDCAN_ConfigFilter(handle_, &filter) != HAL_OK) {
+                Error_Handler();
+            }
+
+            if (HAL_FDCAN_ConfigGlobalFilter(handle_, FDCAN_REJECT, FDCAN_REJECT, FDCAN_REJECT_REMOTE,
+                                             FDCAN_REJECT_REMOTE) != HAL_OK) {
+                Error_Handler();
+            }
+
+            if (HAL_FDCAN_ActivateNotification(handle_, FDCAN_IT_RX_FIFO0_NEW_MESSAGE, 0) != HAL_OK) {
+                Error_Handler();
+            }
+
+            if (HAL_FDCAN_Start(handle_) != HAL_OK) {
+                Error_Handler();
+            }
+        } else {
+            Error_Handler();
+        }
     }
 
-    // int read(CANMessage &msg)
-    // {
-    // }
+    HAL_StatusTypeDef write(uint32_t id, uint8_t *data, uint32_t size, bool is_extended_frame, bool wait_mailbox_free) {
+        FDCAN_TxHeaderTypeDef tx_header;
+        tx_header.Identifier = id;
+        tx_header.IdType = is_extended_frame ? FDCAN_EXTENDED_ID : FDCAN_STANDARD_ID;
+        tx_header.TxFrameType = FDCAN_DATA_FRAME;
+        tx_header.DataLength = (size > 8 ? 8 : size);
+        tx_header.ErrorStateIndicator = FDCAN_ESI_ACTIVE;
+        tx_header.BitRateSwitch = FDCAN_BRS_OFF;
+        tx_header.FDFormat = FDCAN_CLASSIC_CAN;
+        tx_header.TxEventFifoControl = FDCAN_NO_TX_EVENTS;
+        tx_header.MessageMarker = 0;
 
-    // 受信割り込み
-    void attach(std::function<CallbackFnType> &&fn, uint8_t priority = 100) {
-        callback::attach(reinterpret_cast<intptr_t>(handle_), std::move(fn), priority);
+        while (wait_mailbox_free && HAL_FDCAN_GetTxFifoFreeLevel(handle_) == 0)
+            ;
+
+        return HAL_FDCAN_AddMessageToTxFifoQ(handle_, &tx_header, data);
     }
+#endif
 
 private:
-    CAN_HandleTypeDef *handle_;
+    CanHandleType *handle_;
 };
 
 } // namespace stmbed
