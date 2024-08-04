ed1bc1a337ca6e91db73c32a866b2ae2f4902e8d
Kotakku <kotakkucu@gmail.com>
2024-08-04 20:59:58 +0900

    SPIのスレーブを追加

---
+++ callbacks/spi_slave_callback.cpp
@@ -0,0 +1,15 @@
+#include "../spi_slave.hpp"
+
+#ifdef HAL_SPI_MODULE_ENABLED
+
+void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi) {
+    using namespace stmbed;
+    callback::callback<SPISlave::DMACallbackFnType>(reinterpret_cast<intptr_t>(hspi));
+}
+
+void HAL_SPI_ErrorCallback(SPI_HandleTypeDef *hspi) {
+    using namespace stmbed;
+    callback::callback<SPISlave::DMACallbackFnType>(reinterpret_cast<intptr_t>(hspi));
+}
+
+#endif // HAL_SPI_MODULE_ENABLED
